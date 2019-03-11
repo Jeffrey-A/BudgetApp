53dd015685aa5f3c12f836a8b979fe5e3b4323a3
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-03-10 22:40:52 -0400

    js added

--- js/script.js
+++ js/script.js
@@ -3,20 +3,37 @@ var $amount = $("input[type=number]");;
 var $addButton = $("#addButton");
 var $ulIncome = $("#income-sources");
 var $ulExpenses = $("#expenses-sources");
+var $options = $("#select-menu option")
 
 
-
-function createIncomeElement(text,amount ){
-    $ulIncome.append("<li>"+$text.val()+"<span>+"+$amount.val()+"</span>"+"</li>");
+function clearInput(){
+    $text.val("");
+    $amount.val("");
 }
 
-function createExpensesElement(text,amount ){
+function createIncomeElement($text,$amount ){
+    $ulIncome.append("<li>"+$text.val()+"<span>+"+$amount.val()+"</span>"+"</li>");
+    clearInput();
+}
+
+function createExpensesElement($text,$amount ){
     $ulExpenses.append("<li>"+$text.val()+"<span>-"+$amount.val()+"</span>"+"</li>");
+    clearInput();
 }
 
 
 
 
 $addButton.on('click',function(){
-    createExpensesElement($text,$amount);
+    // createExpensesElement($text,$amount);
+    for(var i=0; i<$options.length; i+=1){
+        if ($options[i].selected && $($options[i]).val() === "plus"){
+            createIncomeElement($text,$amount);
+            break;
+        }else{
+            createExpensesElement($text,$amount);
+            break;
+        }
+       
+    }
 });
