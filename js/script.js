840e0c346c50d51cbd1d72702d4378aded22d69e
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-03-10 23:48:25 -0400

    js added

--- js/script.js
+++ js/script.js
@@ -4,7 +4,41 @@ var $addButton = $("#addButton");
 var $ulIncome = $("#income-sources");
 var $ulExpenses = $("#expenses-sources");
 var $options = $("#select-menu option")
+var $totalIncome = $("#totalIncome");
+var $totalExpenses = $("#totalExpenses");
 
+var $finalIncome = $("#finalIncome");
+
+var $incomeValues = $("#income-sources span");
+var $expensesValues = $("#expenses-sources span");
+
+
+
+
+function getTotal($array){
+    var total =0;
+    $array.each(function(){
+        total += parseInt($(this).text());
+    });
+
+    return total;
+}
+
+var totalIncomeSum;
+var totalExpensesSum;
+var totalIncomeValue;
+var totalExpensesValue
+function setUp(){
+    totalIncomeSum = getTotal($incomeValues);
+    totalExpensesSum = getTotal($expensesValues);
+    $totalIncome.text(totalIncomeSum);
+    $totalExpenses.text(totalExpensesSum);
+    totalIncomeValue = parseInt($totalIncome.text());
+    totalExpensesValue = parseInt($totalExpenses.text());
+    $finalIncome.text(""+(totalIncomeValue - totalExpensesValue));
+}
+
+setUp();
 
 function clearInput(){
     $text.val("");
@@ -12,20 +46,16 @@ function clearInput(){
 }
 
 function createIncomeElement($text,$amount ){
-    $ulIncome.append("<li>"+$text.val()+"<span>+"+$amount.val()+"</span>"+"</li>");
+    $ulIncome.append("<li>"+$text.val()+"+<span>"+$amount.val()+"</span>"+"</li>");
     clearInput();
 }
 
 function createExpensesElement($text,$amount ){
-    $ulExpenses.append("<li>"+$text.val()+"<span>-"+$amount.val()+"</span>"+"</li>");
+    $ulExpenses.append("<li>"+$text.val()+"-<span>"+$amount.val()+"</span>"+"</li>");
     clearInput();
 }
 
-
-
-
-$addButton.on('click',function(){
-    // createExpensesElement($text,$amount);
+function main(){
     for(var i=0; i<$options.length; i+=1){
         if ($options[i].selected && $($options[i]).val() === "plus"){
             createIncomeElement($text,$amount);
@@ -34,6 +64,20 @@ $addButton.on('click',function(){
             createExpensesElement($text,$amount);
             break;
         }
-       
+    }
+    setUp();
+    
+    
+}
+
+
+$addButton.on('click',function(){
+    main();  
+});
+
+$amount.on('keypress', function(e){
+    var key = e.keyCode;
+    if (key == 13){
+        main();
     }
 });
